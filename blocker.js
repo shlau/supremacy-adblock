4c67f11932e80dda024f896fcfd8dd5ffe076a62
sheldon <sheldonlau1@gmail.com>
2024-07-15 19:42:34 -0700

    fixed access to elements in iframe

--- blocker.js
+++ blocker.js
@@ -1,9 +1,40 @@
+// ==UserScript==
+// @name Supremacy-adblock
+// @namespace https://raw.githubusercontent.com/shlau/supremacy-adblock/main/blocker.js
+// @match            *://www.supremacy1914.com/*
+// @version          1.0
+// @noframes
+// @grant    GM_getValue
+// @grant    GM_setValue
+// @grant    GM_addStyle
+// ==/UserScript==
 (function () {
-  document.querySelector("#ultimate").width = "100%";
-  const adImages = document.querySelectorAll(
-    'img[src*="static1.bytro.com/fileadmin/templates"]'
-  );
-  adImages.parentElement.style.display = "none";
-  document.querySelector("#inGameAdsContainer").style.display = "none";
-  document.querySelector("#mapContainer canvas").style.width = "100%";
+  const hideAds = () => {
+    const adImages = document.querySelectorAll(
+      'img[src*="static1.bytro.com/fileadmin/templates"]'
+    );
+    if (adImages && adImages.length > 0) {
+      adImages[0].parentElement.style.display = "none";
+    }
+
+    const gameFrame = document.querySelector("#ifm")?.contentWindow.document;
+    if (gameFrame) {
+      const gameContainer = gameFrame.querySelector("#ultimate");
+      if (gameContainer) {
+        gameContainer.style.width = "100%";
+      }
+
+      const adsContainer = gameFrame.querySelector("#inGameAdsContainer");
+      if (adsContainer) {
+        adsContainer.style.display = "none";
+      }
+
+      const mapContainer = gameFrame.querySelector("#mapContainer canvas");
+      if (mapContainer) {
+        mapContainer.style.width = "100%";
+      }
+    }
+  };
+
+  window.setInterval(() => hideAds(), 1000);
 })();
